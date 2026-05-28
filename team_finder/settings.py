c3b27e70a0930e98b8b9f72619b64910eb12b045
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 16:43:33 +0300

    refactor: fix settings.py - remove TASK_VERSION, use named URLs, fix TEMPLATES DIRS

--- team_finder/settings.py
+++ team_finder/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -33,7 +34,7 @@ ROOT_URLCONF = "team_finder.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / f"templates_var{config('TASK_VERSION', default='1')}"],
+        "DIRS": [BASE_DIR / "templates_var1"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -59,9 +60,9 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = "users.User"
-LOGIN_URL = "/users/login/"
-LOGIN_REDIRECT_URL = "/projects/list/"
-LOGOUT_REDIRECT_URL = "/users/login/"
+LOGIN_URL = "users:login"
+LOGIN_REDIRECT_URL = "projects:project_list"
+LOGOUT_REDIRECT_URL = "users:login"
 
 AUTH_PASSWORD_VALIDATORS = []
 if not DEBUG:
