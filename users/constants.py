5231a4a631e9bd0aea3419c39a0e4a6dd5f8c8a5
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 16:36:54 +0300

    refactor: add users/constants.py

---
+++ users/constants.py
@@ -0,0 +1,19 @@
+USER_NAME_MAX_LENGTH = 124
+USER_PHONE_MAX_LENGTH = 12
+USER_ABOUT_MAX_LENGTH = 256
+
+AVATAR_SIZE = 200
+AVATAR_FONT_SIZE = 80
+AVATAR_FONT_PATH = '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf'
+AVATAR_TEXT_COLOR = 'white'
+
+AVATAR_PALETTE = [
+    '#01696f',
+    '#4f98a3',
+    '#6daa45',
+    '#da7101',
+    '#a86fdf',
+    '#d19900',
+    '#dd6974',
+    '#006494',
+]
