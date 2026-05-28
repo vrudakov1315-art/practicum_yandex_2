7348370283dcbb909216121c6d590ca208e44941
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 16:41:17 +0300

    refactor: fix users/admin.py - imports

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from .models import User
+
+from users.models import User
 
 
 @admin.register(User)
