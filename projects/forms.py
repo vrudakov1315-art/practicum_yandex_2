98e571369a139bce247c261a65851cbf3fd02f87
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 16:34:03 +0300

    refactor: fix projects/forms.py - imports, add github_url validation

--- projects/forms.py
+++ projects/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Project
+
+from projects.models import Project
 
 
 class ProjectForm(forms.ModelForm):
@@ -7,3 +8,9 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = ['name', 'description', 'github_url', 'status']
         widgets = {'description': forms.Textarea(attrs={'rows': 5})}
+
+    def clean_github_url(self):
+        url = self.cleaned_data.get('github_url')
+        if url and 'github.com' not in url:
+            raise forms.ValidationError('Укажите ссылку на github.com')
+        return url
