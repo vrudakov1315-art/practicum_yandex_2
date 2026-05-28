d1c44e3a41de52350d4afd77c7af6e70f7e33652
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 17:25:03 +0300

    refactor: fix team_finder/urls.py - PEP8 import order, trailing newline

--- team_finder/urls.py
+++ team_finder/urls.py
@@ -1,12 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="/projects/list/", permanent=False)),
-    path("projects/", include("projects.urls")),
-    path("users/", include("users.urls")),
+    path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='/projects/list/', permanent=False)),
+    path('projects/', include('projects.urls')),
+    path('users/', include('users.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
