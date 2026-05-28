8d0976945db48aaf97195f58b1785ecb58679902
vrudakov1315-art <vrudakov1315@gmail.com>
2026-05-28 17:22:26 +0300

    refactor: use StrEnum for AvatarColor palette in constants.py

--- users/constants.py
+++ users/constants.py
@@ -1,3 +1,5 @@
+from enum import StrEnum
+
 USER_NAME_MAX_LENGTH = 124
 USER_PHONE_MAX_LENGTH = 12
 USER_ABOUT_MAX_LENGTH = 256
@@ -7,13 +9,16 @@ AVATAR_FONT_SIZE = 80
 AVATAR_FONT_PATH = '/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf'
 AVATAR_TEXT_COLOR = 'white'
 
-AVATAR_PALETTE = [
-    '#01696f',
-    '#4f98a3',
-    '#6daa45',
-    '#da7101',
-    '#a86fdf',
-    '#d19900',
-    '#dd6974',
-    '#006494',
-]
+
+class AvatarColor(StrEnum):
+    TEAL = '#01696f'
+    GREEN = '#4f98a3'
+    OLIVE = '#6daa45'
+    ORANGE = '#da7101'
+    PURPLE = '#a86fdf'
+    RED = '#d19900'
+    YELLOW = '#dd6974'
+    DARK_TEAL = '#006494'
+
+
+AVATAR_PALETTE = list(AvatarColor)
